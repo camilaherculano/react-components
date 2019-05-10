d8892a5eb0c96e5ebbaaa00a9794894868e8b22a
Camila Herculano <camilaherculano@users.noreply.github.com>
2019-05-10 14:54:00 -0400

    Playing around with components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = () => {
+    return (
+        <div className="ui container comments">
+        <ApprovalCard>
+            <div><h4>alo</h4>alo alo</div>
+        </ApprovalCard>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    timeAgo="Today at 4:45AM" 
+                    content="Nice job!" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="John" 
+                    timeAgo="Today at 1:02PM" 
+                    content="I'm following you right now!" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Camila" 
+                    timeAgo="Today at 3:40PM" 
+                    content="CHAMPS" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Maria" 
+                    timeAgo="Today at 4:45PM" 
+                    content="Ok then.." 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Hercules" 
+                    timeAgo="Today at 4:46PM" 
+                    content="you, show off!" 
+                    avatar={faker.image.avatar()}
+                />
+            </ApprovalCard>
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
